38c05e43781dba2270d4b003488af6602293c2f7
Connor Clark <cjamcl@gmail.com>
2022-10-22 19:33:32 -0700

    wip

---
+++ build.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -e
+
+if ! command -v emsdk &> /dev/null
+then
+  echo "emsdk could not be found."
+  echo "https://emscripten.org/docs/getting_started/downloads.html"
+  exit 1
+fi
+
+EMCC_VERSION=3.1.24
+emsdk install $EMCC_VERSION
+emsdk activate $EMCC_VERSION
+source emsdk_env.sh
+
+rm -rf build || true
+mkdir build
+cd build
+
+EMCC_FLAGS=(
+  -s USE_SDL=2
+  -s USE_PTHREADS=1
+  -I "$EMCC_CACHE_INCLUDE_DIR/AL"
+)
+LINKER_FLAGS=(
+  --shared-memory
+  -lpthread
+)
+
+emcmake cmake \
+  -G "Ninja Multi-Config" \
+  -D ALLEGRO_SDL=ON \
+  -D WANT_ALLOW_SSE=OFF \
+  -D WANT_OPENAL=OFF \
+  -D WANT_ALSA=OFF \
+  -D SDL2_INCLUDE_DIR="$EMCC_CACHE_INCLUDE_DIR" \
+  -D SDL2_LIBRARY="$EMCC_CACHE_LIB_DIR/libSDL2-mt.a" \
+  -D CMAKE_C_FLAGS="${EMCC_FLAGS[*]}" \
+  -D CMAKE_CXX_FLAGS="${EMCC_FLAGS[*]}" \
+  -D CMAKE_EXE_LINKER_FLAGS="${LINKER_FLAGS[*]}" \
+  ..
+
+cmake --build . --config Debug -t app
